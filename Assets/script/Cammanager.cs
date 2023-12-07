68dd06e953b4f5e533e433aba6063422502f803b
bcgkky <bcgkky1@gmail.com>
2023-12-07 12:34:58 +0300

    last
    ...

---
+++ Assets/script/Cammanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cammanager : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+    public Camera cam3;
+    
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            cam1.enabled = false;
+            cam3.enabled = false;
+            cam2.enabled = true;
+            cam2.fieldOfView = 10f;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            cam1.enabled = false;
+            cam3.enabled = true;
+            cam2.enabled = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cam1.enabled = true;
+            cam3.enabled = false;
+            cam2.enabled = false;
+        }
+    }
+}
---
+++ Assets/script/Efektlerscript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Efektlerscript : MonoBehaviour
+{
+    /*TrailRenderer trailefekt;
+    public int speed;*/
+
+    LineRenderer line;
+    public Transform hedef;
+
+    void Start()
+    {
+        /*speed = 1;
+        trailefekt = GetComponentInChildren<TrailRenderer>();*/
+        line = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, hedef.position);
+
+
+        /*if (speed < 5)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            trailefekt.time = 3f;
+        }
+        else
+        {
+            transform.Translate(Vector3.right * speed * 2 * Time.deltaTime);
+            trailefekt.time = .1f;
+            trailefekt.startColor = new Color(.93f, 1f, .47f);
+            trailefekt.endColor = new Color(.43f, 1f, .75f);
+            //trailefekt.startColor = Color.white;
+            //trailefekt.endColor = Color.yellow;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * 10f * 2 * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * 10f * 2 * Time.deltaTime);
+        }
+        */
+    }
+}
